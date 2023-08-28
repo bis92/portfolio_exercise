6921bddc16397cde4078d6c2a8f871ca337b9a39
Bis92 <bis920309@gmail.com>
2023-08-28 23:00:13 +0900

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+const header = document.querySelector('.header');
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", (event) => {
+  if(window.scrollY > headerHeight) {
+    header.classList.add('header--dark');
+  } else {
+    header.classList.remove('header--dark');
+  }
+});
