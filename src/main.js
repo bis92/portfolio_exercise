7d5c53caf6567b7cf96b4cb341109a00e0fbd44d
Bis92 <bis920309@gmail.com>
2023-08-28 23:29:44 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 
 const header = document.querySelector('.header');
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
+
 document.addEventListener("scroll", (event) => {
   if(window.scrollY > headerHeight) {
     header.classList.add('header--dark');
   } else {
     header.classList.remove('header--dark');
   }
+  home.style.opacity = (home.offsetHeight-window.scrollY)/home.offsetHeight
+});
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", (event) => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
 });
