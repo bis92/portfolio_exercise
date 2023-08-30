879acc4a0a1eec94d7a89b6b3324a6147605bfa4
Bis92 <bis920309@gmail.com>
2023-08-31 00:20:15 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -27,3 +27,14 @@ document.addEventListener("scroll", (event) => {
   }
 });
 
+// Navbar  토글 버튼 클릭 처리
+const navbarMenu = document.querySelector('.header__menu');
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+})
+
+navbarMenu.addEventListener('click', () => {
+  navbarMenu.classList.remove('open');
+})
+
